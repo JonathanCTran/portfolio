7dcf4155874a2bd2deb00234a8485e77776867d9
Jonathan Tran <jct92@drexel.edu>
2025-01-17 00:04:33 -0500

    update app.css

--- src/App.js
+++ src/App.js
@@ -12,22 +12,23 @@ class App extends Component {
 
       <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
-        {/* Clickable profile image */}
-        <a
-          href="https://drive.google.com/file/d/1qmjgoDiGu-MElhmRjcxegR4O_JRQv-5z/view?usp=drive_link"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-        <img 
-          src="https://media.licdn.com/dms/image/v2/D4E03AQFKq1xAhaRovw/profile-displayphoto-shrink_400_400/B4EZRx_GqlG0Ao-/0/1737079156186?e=1742428800&v=beta&t=2cpI52TQL4oVWl6uPb-qJJ5c1Y-RLA2qUxtFLgdiRrg" 
-          alt="Jonathan Tran's profile" 
-          className="user-image" 
-        />
-        </a>
+       
           
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">From Concept to Deployment | Jonathan Tran’s Tech Journey</h1>
+           {/* Clickable profile image */}
+          <a
+            href="https://drive.google.com/file/d/1qmjgoDiGu-MElhmRjcxegR4O_JRQv-5z/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+          <img 
+            src="https://media.licdn.com/dms/image/v2/D4E03AQFKq1xAhaRovw/profile-displayphoto-shrink_400_400/B4EZRx_GqlG0Ao-/0/1737079156186?e=1742428800&v=beta&t=2cpI52TQL4oVWl6uPb-qJJ5c1Y-RLA2qUxtFLgdiRrg" 
+            alt="Jonathan Tran's profile" 
+            className="user-image" 
+          />
+          </a>
         </header>
 
     
